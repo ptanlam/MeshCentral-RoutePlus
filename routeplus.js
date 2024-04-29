1b3c36419808bdcda6836ea1311291e8c68aca5d
lam.phan <lam.phan@opswat.com>
2024-04-29 08:57:49 +0700

    change logic when setting new computer

--- routeplus.js
+++ routeplus.js
@@ -343,22 +343,22 @@ module.exports.routeplus = function (parent) {
       case 'setMyComputer':
         // remove mappings from current computer first
         obj.db
-          .getMyComputer(command.user)
-          .then((mys) => {
-            if (mys.length == 0) return Promise.resolve();
-            var my = mys[0];
-            return obj.db
-              .getUserMaps(mys[0].user)
-              .then((maps) => {
-                obj.debug('PLUGIN', 'RoutePlus', 'setMyComputer maps length: ' + maps.length);
-                if (maps.length == 0) return Promise.resolve();
-                maps.forEach(function (map) {
-                  obj.endRoute(map._id);
-                });
-                return Promise.resolve();
-              })
-              .catch((e) => console.log('PLUGIN: RoutePlus: setMyComputer user map changing failed with: ', e));
-          })
+          // .getMyComputer(command.user)
+          // .then((mys) => {
+          //   if (mys.length == 0) return Promise.resolve();
+          //   var my = mys[0];
+          //   return obj.db
+          //     .getUserMaps(mys[0].user)
+          //     .then((maps) => {
+          //       obj.debug('PLUGIN', 'RoutePlus', 'setMyComputer maps length: ' + maps.length);
+          //       if (maps.length == 0) return Promise.resolve();
+          //       maps.forEach(function (map) {
+          //         obj.endRoute(map._id);
+          //       });
+          //       return Promise.resolve();
+          //     })
+          //     .catch((e) => console.log('PLUGIN: RoutePlus: setMyComputer user map changing failed with: ', e));
+          // })
           .then(() => {
             return obj.db.setMyComputer({
               user: command.user,
